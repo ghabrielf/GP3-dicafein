0848237d4e1aadb36f7451726a3d7dd0ffeca756
Abdul Latif Mubasir <bj.angel119@gmail.com>
2021-09-29 09:43:23 +0700

    chore: update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const handleError = require('./middlewares/handleError')
 const app = express()
-
 const port = 3000
+const routers = require('./routers')
 
+app.use(express.urlencoded({ extended: true }))
+
+app.use(routers)
+
+app.use(handleError)
 app.listen(port , () => {
     `server running at port ${port}`
 })
