fb997611a0e15efefa9cdb5e8ac9fd6b9c8d2ba6
ghabrielf <ghabrielfebryann@gmail.com>
2021-10-01 15:08:13 +0700

    feat:update table controllers

--- controllers/table.js
+++ controllers/table.js
@@ -5,7 +5,7 @@ const { Table } = require('../models')
 class tableController {
     static getAll = async (req, res, next) => {
         try{
-            const tables = await Table.findAll()
+            const tables = await Table.findAll({ attributes: ['id' , 'chalk_board' , 'status'] })
             res.status(200).json(tables)
 
         }catch (error) {
@@ -15,8 +15,8 @@ class tableController {
     }
     static create = async (req, res, next) => {
         try{
-            const {table_number, status} = req.body
-            const tables = await Table.create({table_number, status})
+            const {chalk_board, status} = req.body
+            const tables = await Table.create({chalk_board, status})
             res.status(201).json(tables)
         }catch (error) {
             next (error)
@@ -26,6 +26,7 @@ class tableController {
         try{
             const {id} = req.params
             const tables = await Table.findByPk(id)
+            if(!tables) return next({name: "NotFound"})
             res.status(200).json(tables)
 
         }catch (error) {
@@ -34,12 +35,14 @@ class tableController {
     }
     static update = async (req, res, next) => {
         try{
-            const {table_number, status} = req.body
+            const {chalk_board, status} = req.body
             const{id} = req.params
-            const tables = await Table.findByPk(id)
-            tables.update({table_number, status})
+            const cafe_table = await Table.findByPk(id)
+            if(!cafe_table) return next({name: "NotFound"})
+            cafe_table.update({chalk_board, status})
             res.status(200).json({
-                message: 'Update Success'
+                message: 'Update Success',
+                cafe_table
             })
         } catch (error) {
             next(error)
@@ -49,11 +52,25 @@ class tableController {
         try{
             let {id} = req.params
             const tables = await Table.findByPk(id)
+            if(!tables) return next({name: "NotFound"})
             tables.destroy()
             res.status(200).json({
                 message: 'Delete Success'
             })
 
+        }catch (error) {
+            next(error)
+        }
+    }
+    static update_status = async (req, res, next) => {
+        try{
+            const {status} = req.body
+            const {id} = req.params
+            const tables = await Table.findByPk(id)
+            tables.update({status})
+            res.status(200).json({
+                message: 'Success update'
+            })
         }catch (error) {
             next(error)
         }
