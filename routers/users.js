f6770efb6ca16467a91e8e177cb95cfe4c832be9
ghabrielf <ghabrielfebryann@gmail.com>
2021-10-01 15:17:12 +0700

    feat:update users.js routers

--- routers/users.js
+++ routers/users.js
@@ -4,10 +4,13 @@ const router = require ('express').Router();
 const authController = require ('../controllers/auth')
 const authentication = require('../middlewares/authentication')
 const authorization = require('../middlewares/authorization')
+const userMiddleware = require('../middlewares/userMiddleware')
+const {validate} = require('express-validation')
+const validation = require('../validation/usersValidation')
 
-router.post('/users/register' , authController.register)
-router.post('/users/login' , authController.login)
+router.post('/users/register' , validate(validation.register() , {} , {}) , authController.register)
+router.post('/users/login' , validate(validation.login() , {} , {}) , authController.login)
 router.get('/users', authentication , authorization(['admin']), authController.getAll )
-router.get('/users/:id', authentication , authorization(['admin']), authController.getDetail)
+router.get('/users/:id', authentication , userMiddleware , authController.getDetail)
 
 module.exports = router
